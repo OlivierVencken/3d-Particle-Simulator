259edbf38114388495f96170cdc637317174a2fa
OlivierVencken <venckenolivier@gmail.com>
2026-07-13 16:05:44 +0200

    ui: updated colors

--- src/main/java/com/particle/sim/ui/theme/UIColors.java
+++ src/main/java/com/particle/sim/ui/theme/UIColors.java
@@ -4,32 +4,32 @@ public final class UIColors {
     public static final UIColor TRANSPARENT = UIColor.fromHex("#00000000");
     public static final UIColor SCRIM = UIColor.fromHex("#00000099");
 
-    public static final UIColor TEXT_PRIMARY = UIColor.fromHex("#E7E9EC");
-    public static final UIColor TEXT_MUTED = UIColor.fromHex("#8F98A3");
-    public static final UIColor TEXT_SELECTION = UIColor.fromHex("#56606C66");
+    public static final UIColor TEXT_PRIMARY = UIColor.fromHex("#E8E8E8");
+    public static final UIColor TEXT_MUTED = UIColor.fromHex("#9A9A9A");
+    public static final UIColor TEXT_SELECTION = UIColor.fromHex("#68686866");
 
-    public static final UIColor BACKGROUND_WINDOW = UIColor.fromHex("#0B0D10FA");
-    public static final UIColor BACKGROUND_PANEL = UIColor.fromHex("#11151A");
-    public static final UIColor BACKGROUND_POPUP = UIColor.fromHex("#171C22FA");
-    public static final UIColor BACKGROUND_MENU = UIColor.fromHex("#11151A");
-    public static final UIColor BACKGROUND_TITLE = UIColor.fromHex("#11151A");
-    public static final UIColor BACKGROUND_TITLE_ACTIVE = UIColor.fromHex("#171C22");
-    public static final UIColor BACKGROUND_TITLE_COLLAPSED = UIColor.fromHex("#0B0D10E6");
+    public static final UIColor BACKGROUND_WINDOW = UIColor.fromHex("#1818186a");
+    public static final UIColor BACKGROUND_PANEL = UIColor.fromHex("#202020");
+    public static final UIColor BACKGROUND_POPUP = UIColor.fromHex("#2c2c2cfa");
+    public static final UIColor BACKGROUND_MENU = UIColor.fromHex("#202020");
+    public static final UIColor BACKGROUND_TITLE = UIColor.fromHex("#202020");
+    public static final UIColor BACKGROUND_TITLE_ACTIVE = UIColor.fromHex("#2C2C2C");
+    public static final UIColor BACKGROUND_TITLE_COLLAPSED = UIColor.fromHex("#181818E6");
 
-    public static final UIColor BORDER_DEFAULT = UIColor.fromHex("#2A313A");
-    public static final UIColor BORDER_STRONG = UIColor.fromHex("#3A434E");
-    public static final UIColor BORDER_SUBTLE = UIColor.fromHex("#20262E");
+    public static final UIColor BORDER_DEFAULT = UIColor.fromHex("#404040");
+    public static final UIColor BORDER_STRONG = UIColor.fromHex("#5A5A5A");
+    public static final UIColor BORDER_SUBTLE = UIColor.fromHex("#343434");
 
-    public static final UIColor SURFACE_DEFAULT = UIColor.fromHex("#171C22");
-    public static final UIColor SURFACE_HOVER = UIColor.fromHex("#202730");
-    public static final UIColor SURFACE_ACTIVE = UIColor.fromHex("#2A313A");
+    public static final UIColor SURFACE_DEFAULT = UIColor.fromHex("#292929");
+    public static final UIColor SURFACE_HOVER = UIColor.fromHex("#353535");
+    public static final UIColor SURFACE_ACTIVE = UIColor.fromHex("#424242");
 
-    public static final UIColor CONTROL_DEFAULT = UIColor.fromHex("#171C22");
-    public static final UIColor CONTROL_HOVER = UIColor.fromHex("#252D36");
-    public static final UIColor CONTROL_ACTIVE = UIColor.fromHex("#3A434E");
+    public static final UIColor CONTROL_DEFAULT = UIColor.fromHex("#292929");
+    public static final UIColor CONTROL_HOVER = UIColor.fromHex("#383838");
+    public static final UIColor CONTROL_ACTIVE = UIColor.fromHex("#505050");
 
-    public static final UIColor TABLE_HEADER = UIColor.fromHex("#141414");
-    public static final UIColor TABLE_ROW_ALTERNATE = UIColor.fromHex("#1B1B1B59");
+    public static final UIColor TABLE_HEADER = UIColor.fromHex("#262626");
+    public static final UIColor TABLE_ROW_ALTERNATE = UIColor.fromHex("#FFFFFF0A");
 
     public static final UIColor INTERACTION_ATTRACTION = UIColor.fromHex("#33FF40");
     public static final UIColor INTERACTION_NEUTRAL = UIColor.fromHex("#8C8C8C");
